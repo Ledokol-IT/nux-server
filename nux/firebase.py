e12b7343035bbffb3edc62ca497d55ade475b019
i3cheese <i3cheese@yandex.ru>
2022-07-20 17:30:58 +0700

    "enable debug logging"

--- nux/firebase.py
+++ nux/firebase.py
@@ -14,13 +14,16 @@ firebase_app = None
 
 def setup_firebase(options):
     global firebase_app, DRY_RUN, DISABLED
+    logging.info("Setup firebase start")
     if options.google_creds:
+        logging.info("Setup firebase with base64 encoded string")
         creds = base64.b64decode(options.google_creds)
         creds = json.loads(creds)
         credentials = firebase_admin.credentials.Certificate(
             creds,
         )
     elif options.google_creds_file:
+        logging.info(f"Setup firebase with file {options.google_creds_file}")
         credentials = firebase_admin.credentials.Certificate(
             options.google_creds_file,
         )
@@ -33,12 +36,16 @@ def setup_firebase(options):
 
     if firebase_app is None:
         firebase_app = firebase_admin.initialize_app(credentials)
+        logging.info("Firebase initialized succesfuly")
+    else:
+        logging.info("Firebase was already initialized")
     DISABLED = False
 
 
 def send_messages(
     messages: list[firebase_admin.messaging.Message],
 ):
+    logging.debug(f"sent {len(messages)} messages")
     firebase_admin.messaging.send_all(
         messages,
         app=firebase_app,
